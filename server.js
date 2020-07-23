3acfea2840fee3cdb6bba4f340d67a341dd5f784
FindPecker <ayodeleayoola264@gmail.com>
2020-07-22 23:39:16 -0400

    Server modified

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const path = require('path');
 const app = express();
 
 app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
