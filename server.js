dc57c99e5d51bced22af58583c191f7207518517
FindPecker <ayodeleayoola264@gmail.com>
2020-07-22 22:12:08 -0400

    Server modified

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const compression = require('compression');
 const path = require('path');
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
